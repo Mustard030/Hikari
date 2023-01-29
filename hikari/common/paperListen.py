105b746dcb245f8b50ff1dc67b79bba31b080fcc
jiemoo <978090944@qq.com>
2023-01-29 16:20:19 +0800

    paperListen.py except PyperclipWindowsException

--- hikari/common/paperListen.py
+++ hikari/common/paperListen.py
@@ -1,15 +1,19 @@
 import pyperclip
 from hikari.event import loop
-from hikari.common.parse import parse
+from hikari.common.parse import parse_paperclip
+from hikari.config.hikari_config import config
 
 
 def paperclip(last=None):
 	if last is None:
 		last = pyperclip.paste().strip()
 
-	data = pyperclip.paste().strip()
+	try:
+		data = pyperclip.paste().strip()
+	except pyperclip.PyperclipWindowsException:
+		loop.call_later(0.1, paperclip)
+	else:
+		if last != data and config.listen_paperclip:
+			loop.create_task(parse_paperclip(data))
 
-	if last != data:
-		loop.create_task(parse(data))
-
-	loop.call_later(0.1, paperclip, data)
+		loop.call_later(0.1, paperclip, data)
