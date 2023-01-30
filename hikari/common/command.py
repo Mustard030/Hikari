8d33d35de1c55130261ff42ced89ea457d101dc2
jiemoo <978090944@qq.com>
2023-01-31 01:10:33 +0800

    Processes instructions entered by the user

---
+++ hikari/common/command.py
@@ -0,0 +1,19 @@
+###############################################################
+# 输入命令的执行函数静态类，归纳所有执行命令的函数
+###############################################################
+import logging
+
+from hikari.config.hikari_config import config
+
+
+class Command:
+	@staticmethod
+	def changePaperclipListening():
+		setting = config.listen_paperclip
+		setting = not setting
+		config.listen_paperclip = setting
+		logging.info(f"剪切板监听: {setting}")
+
+	@staticmethod
+	def doNothing():
+		pass
