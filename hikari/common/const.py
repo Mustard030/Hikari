c1aebc9dadcc355a8d56b83242de9886eeed24ec
jiemoo <978090944@qq.com>
2023-01-31 01:03:57 +0800

    const

--- hikari/common/const.py
+++ hikari/common/const.py
@@ -1,2 +1,11 @@
+DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+ACCEPT_LANGUAGE = "zh-CN,zh;q=0.9"
+
+PIXIV_INFO_URL = "https://www.pixiv.net/ajax/illust/{pixiv_id}"
+PIXIV_PROXY = "https://pixiv.re/"
+PIXIV_PROXY_WITH_P = PIXIV_PROXY + "{pixiv_id}-{p}.jpg"
+PIXIV_PROXY_WITHOUT_P = PIXIV_PROXY + "{pixiv_id}.jpg"
 
 
+TWITTER_DOMAIN = ".twitter.com"
+TWEET_CONTENT = 'https://api.twitter.com/graphql/{query_id}/TweetDetail?variables=%7B%22focalTweetId%22%3A%22{tweet_id}%22%2C%22with_rux_injections%22%3Afalse%2C%22includePromotedContent%22%3Atrue%2C%22withCommunity%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withBirdwatchNotes%22%3Atrue%2C%22withSuperFollowsUserFields%22%3Atrue%2C%22withDownvotePerspective%22%3Afalse%2C%22withReactionsMetadata%22%3Afalse%2C%22withReactionsPerspective%22%3Afalse%2C%22withSuperFollowsTweetFields%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D&features=%7B%22responsive_web_twitter_blue_verified_badge_is_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22view_counts_public_visibility_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22responsive_web_uc_gql_enabled%22%3Atrue%2C%22vibe_api_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Afalse%2C%22interactive_text_enabled%22%3Atrue%2C%22responsive_web_text_conversations_enabled%22%3Afalse%2C%22responsive_web_enhance_cards_enabled%22%3Atrue%7D'
