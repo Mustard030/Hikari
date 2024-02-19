d29a4b38673fb90af49bd2bcced307c0eab8a369
jiemoo <978090944@qq.com>
2024-02-19 15:07:17 +0800

    修改为策略模式匹配链接

--- hikari/common/character.py
+++ hikari/common/character.py
@@ -2,64 +2,8 @@ import re
 from typing import Type
 
 from hikari.common import content
-from hikari.common.exceptions import LinktypeNotExistError
+from hikari.common.exceptions import LinktypeNotSupportError
 from hikari.common.linktype import LinkType
-
-
-class _Character:
-    PIXIV = "www.pixiv.net"
-    TWITTER = "twitter.com"
-    TWIMG = "pbs.twimg.com"
-    FANBOX = "fanbox.cc"
-    YANDE = "yande.re"
-    DANBOORU = "danbooru.donmai.us"
-    GELBOORU = "gelbooru.com"
-    KEMONO = "kemono.su"
-    FANTIA = "fantia.jp"
-    character_list = [
-        PIXIV,
-        # TWITTER,
-        TWIMG,
-        FANBOX,
-        YANDE,
-        DANBOORU,
-        GELBOORU,
-        KEMONO,
-        FANTIA
-    ]
-
-    __character_dict = {
-        PIXIV: LinkType.PIXIV,
-        TWITTER: LinkType.TWITTER,
-        TWIMG: LinkType.TWIMG,
-        FANBOX: LinkType.FANBOX,
-        YANDE: LinkType.YANDE,
-        DANBOORU: LinkType.DANBOORU,
-        GELBOORU: LinkType.GELBOORU,
-        KEMONO: LinkType.KEMONO,
-        FANTIA: LinkType.FANTIA,
-    }
-
-    pixiv_re_patterns = [
-        r"https?://www.pixiv.net/artworks/\d{8,9}",
-    ]
-
-    kemono_re_patterns = [
-        r"https?://www.kemono.party/(?P<platform>\w+)/user/(?P<kemono_userid>\w+)/post/(?P<kemono_postid>\w+)",
-    ]
-
-    def match(self, value) -> LinkType:
-        for c in self.character_list:
-            if c in value:
-                return self.__character_dict.get(c, None)
-
-        raise LinktypeNotExistError(value)
-
-    def re_match(self, value) -> LinkType:
-        pass
-
-
-Character = _Character()
 
 
 # 基类
@@ -78,11 +22,11 @@ class BaseMatchingStrategy(object, metaclass=BaseMetaMatchingStrategy):
     linktype = LinkType.NONE
 
     @classmethod
-    def is_match(cls, url):
+    def is_match(cls, url) -> re.Match:
         """
         是否匹配该类链接的格式
         """
-        pass
+        ...
 
     @classmethod
     def get_content(cls, url) -> content.Content:
@@ -97,7 +41,7 @@ class TwimgMatchingStrategy(BaseMatchingStrategy):
     linktype = LinkType.TWIMG
 
     @classmethod
-    def is_match(cls, url):
+    def is_match(cls, url) -> re.Match:
         patterns = [
             r"https?://pbs.twimg.com/media/(.*?)"
         ]
@@ -114,7 +58,7 @@ class PixivMatchingStrategy(BaseMatchingStrategy):
     linktype = LinkType.PIXIV
 
     @classmethod
-    def is_match(cls, url):
+    def is_match(cls, url) -> re.Match:
         patterns = [
             r"https?://www.pixiv.net/artworks/(\d{8,9})",
             r"https?://www.pixiv.net/member_illust.php\?mode=\w+&illust_id=(\d{8,9})",
@@ -132,7 +76,7 @@ class KemonoMatchingStrategy(BaseMatchingStrategy):
     linktype = LinkType.KEMONO
 
     @classmethod
-    def is_match(cls, url):
+    def is_match(cls, url) -> re.Match:
         patterns = [
             r"https://(www.)?kemono.(party|su)/\w+/user/(\d+)/post/(\d+)"
         ]
@@ -149,9 +93,10 @@ class YandeMatchingStrategy(BaseMatchingStrategy):
     linktype = LinkType.YANDE
 
     @classmethod
-    def is_match(cls, url):
+    def is_match(cls, url) -> re.Match:
         patterns = [
-            r"https?://yande.re/post/show/\d+"
+            r"https?://yande.re/post/show/\d+",
+            r"https?://yande.re/post/browse#\d+",
         ]
         for pattern in patterns:
             if res := re.match(pattern, url):
@@ -162,13 +107,31 @@ class YandeMatchingStrategy(BaseMatchingStrategy):
         return content.Yande(url)
 
 
+class GelbooruMatchingStrategy(BaseMatchingStrategy):
+    linktype = LinkType.GELBOORU
+
+    @classmethod
+    def is_match(cls, url) -> re.Match:
+        patterns = [
+            r"https?://gelbooru.com/index.php\?page=post&s=view&id=\d+",
+        ]
+        for pattern in patterns:
+            if res := re.match(pattern, url):
+                return res
+
+    @classmethod
+    def get_content(cls, url):
+        return content.Gelbooru(url)
+
+
 class SankakuMatchingStrategy(BaseMatchingStrategy):
     linktype = LinkType.SANKAKU
 
     @classmethod
-    def is_match(cls, url):
+    def is_match(cls, url) -> re.Match:
         patterns = [
             r"https?://beta.sankakucomplex.com/zh-CN/post/show/(\d+)",
+            r"https?://sankaku.app/post/show/(\d+)"
         ]
         for pattern in patterns:
             if res := re.match(pattern, url):
@@ -179,12 +142,30 @@ class SankakuMatchingStrategy(BaseMatchingStrategy):
         return content.Sankaku(url)
 
 
+class DanbooruMatchingStrategy(BaseMatchingStrategy):
+    linktype = LinkType.DANBOORU
+
+    @classmethod
+    def is_match(cls, url) -> re.Match:
+        patterns = [
+            r"https://danbooru.donmai.us/posts/(\d+)\?q=parent%3A(\d+)",
+            r"https://danbooru.donmai.us/posts/(\d+)",
+        ]
+        for pattern in patterns:
+            if res := re.match(pattern, url):
+                return res
+
+    @classmethod
+    def get_content(cls, url):
+        return content.Danbooru(url)
+
+
 def match_strategy(url) -> Type[BaseMatchingStrategy]:
     for cls in BaseMetaMatchingStrategy.record_cls:
         if cls.is_match(url):
             return cls
 
-    raise LinktypeNotExistError(url)
+    raise LinktypeNotSupportError(url)
 
 # urls = r'https://beta.sankakucomplex.com/zh-CN/post/show/16579876?tags=parent%3A7323385'
 # c = match_strategy(urls)
